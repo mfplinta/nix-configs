8cb340ba3a512cb8e2780b34a806678b5b90acb5
Matheus <mfplinta@gmail.com>
2025-05-19 21:27:00 -0600

    Fix brightness keybindings

--- common/desktop.nix
+++ common/desktop.nix
@@ -115,8 +115,8 @@
                   ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
                   ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
                   ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-                  ", XF86MonBrightnessUp, exec, ${brillo} -e -S $(($(printf '%.0f\n' $(${brillo}))+5))"
-                  ", XF86MonBrightnessDown, exec, ${brillo} -e -S $(($(printf '%.0f\n' $(${brillo}))-5))"
+                  ", XF86MonBrightnessUp, exec, ${brillo} -e -A 5"
+                  ", XF86MonBrightnessDown, exec, ${brillo} -e -U 5"
                 ];
                 bindl = [
                   ", XF86AudioNext, exec, ${playerctl} next"
