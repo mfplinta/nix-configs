6f76f498e24a8cb4cd66e0e7c2a8db578c7be2ae
Matheus <mfplinta@gmail.com>
2025-09-03 19:46:23 -0600

    Reorganize imports

--- common/base.nix
+++ common/base.nix
@@ -32,10 +32,6 @@
           userEmail = "mfplinta@gmail.com";
         };
 
-        home.packages = with pkgs; [
-          galaxy-buds-client
-        ];
-
         home.stateVersion = "24.11";
       };
     };
@@ -50,66 +46,15 @@
       boot.loader.systemd-boot.enable = true;
       boot.loader.timeout = 0;
       boot.loader.efi.canTouchEfiVariables = true;
-      boot.kernelPackages = pkgs.lib.mkDefault pkgs.linuxPackages_cachyos;
-      boot.kernel.sysctl."kernel.printk" = "3 3 3 3";
-      boot.kernelParams = [
-        "quiet"
-        "splash"
-        "loglevel=3"
-        "systemd.show_status=auto"
-        "nosgx"
-        "udev.log_priority=3"
-        "rd.systemd.show_status=auto"
-      ];
       boot.tmp.useTmpfs = true;
-
-      boot.plymouth.enable = true;
-      boot.initrd.verbose = false;
-      boot.initrd.systemd.enable = true;
 
       security.sudo.extraConfig = ''
         Defaults pwfeedback,insults
         Defaults timestamp_timeout=15
       '';
 
-      networking.networkmanager.enable = true;
       time.timeZone = "America/Denver";
-
-      hardware = {
-        graphics.enable = true;
-        bluetooth.enable = true;
-        bluetooth.powerOnBoot = true;
-        bluetooth.settings = {
-          General = {
-            ControllerMode = "bredr";
-          };
-        };
-        brillo.enable = true;
-      };
-
-      # Reduce RAM cache for ejectable devices
-      services.udev.extraRules = ''
-        SUBSYSTEM=="block", ACTION=="add",\
-          KERNEL=="sd[a-z]",\
-          TAG+="systemd",\
-          ENV{ID_USB_TYPE}=="disk",\
-          ENV{SYSTEMD_WANTS}+="usb-dirty-pages-fix@$kernel.service"
-      '';
-      systemd.services."usb-dirty-pages-fix@" = {
-        scriptArgs = "%i";
-        script = ''
-          if [ -z "$(df --output=source '/' | grep $1)" ]; then
-              echo 1 > /sys/block/$1/bdi/strict_limit
-              echo 16777216 > /sys/block/$1/bdi/max_bytes
-          fi
-        '';
-        serviceConfig.Type = "oneshot";
-      };
-
-      programs.appimage = {
-        enable = true;
-        binfmt = true;
-      };
+      i18n.defaultLocale = "en_US.UTF-8";
 
       programs.htop = {
         enable = true;
--- targets/cloudy/configuration.nix
+++ targets/cloudy/configuration.nix
@@ -2,6 +2,7 @@
   pkgs,
   lib,
   config,
+  sysImport,
   ...
 }:
 
@@ -9,11 +10,11 @@
   imports = [
     ./hardware-configuration.nix
     ./disko.nix
+
+    (sysImport ../../common/base.nix)
+    (sysImport ../../common/server.nix)
   ];
 
-  boot.loader.timeout = 1;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelParams = [
     "net.ifnames=0"
     "boot.shell_on_fail"
@@ -70,15 +71,6 @@
         ];
       };
     };
-
-  time.timeZone = "America/Denver";
-
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    git
-    htop
-  ];
 
   systemd.tmpfiles.rules = [
     "d /persist/containers/caddy 0600 root root -"
@@ -182,35 +174,6 @@
       };
     };
 
-  services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDO7lQ5/HFagyOrQKXT9IC1PqBm/Yvi3zThJ7Azawg3lb6FHKUms8FuR5vS5rkrRCaTrs8URHugirIMvXQYxmTMbU51jGsJcWPNfTPm/iWczYUVFHY5TY4BlviWMu0EzHA9pA5bcR8DGjEgJdloPwuQ6eOTda0x9HBNBT8Q0xbiXTjmcYwluqw3iI8Up54f5zR6nC0pMkKTHIjmSLzCnbCBFsZ+aIvtE339oLbQZ5B5Jlw0/lgV1m9/GTn/PUHbUPbgKW3MZ4/kCuqh62UvqdyMa2bjaqPZLfcrNkJSvT8xVxk4enKUfHj5VI1jwG6SJ6s6fc/FUHZJlt3wtGsw08XL ssh-key-2025-09-02"
-  ];
-
-  services.vmagent = {
-    enable = true;
-    remoteWrite = {
-      url = "http://192.168.101.11:8428/api/v1/write";
-    };
-    prometheusConfig = {
-      scrape_configs = [
-        {
-          job_name = "cloudy@node-exporter";
-          scrape_interval = "60s";
-          static_configs = [
-            {
-              targets = [ "127.0.0.1:9100" ];
-              labels.type = "node";
-            }
-          ];
-        }
-      ];
-    };
-  };
-
-  services.prometheus.exporters.node = {
-    enable = true;
-    port = 9100;
-    enabledCollectors = [ "systemd" ];
-  };
+  myCfg.vmagentEnable = true;
+  myCfg.vmagentRemoteWriteUrl = "http://192.168.101.11:8428/api/v1/write";
 }
