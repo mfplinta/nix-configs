c46c9e593a41f6c86148f1df2a46dbffb2882c64
Matheus <mfplinta@gmail.com>
2025-09-18 18:13:53 -0600

    (workstation) Add looking glass

--- common/bundles/development.nix
+++ common/bundles/development.nix
@@ -1,6 +1,6 @@
 {
   hmModule =
-    { pkgs, hmImport, ... }:
+    { pkgs, lib, hmImport, ... }:
     {
       imports = [
         (hmImport ./../programs/kate.nix)
@@ -11,5 +11,9 @@
         jetbrains.pycharm-professional
         inkscape
       ];
+
+      home.activation.createCopilotSymlink = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        run ln -sfn ${pkgs.github-copilot-intellij-agent}/bin/copilot-agent /home/matheus/.local/share/JetBrains/PyCharm2025.1/github-copilot-intellij/copilot-agent/native/linux-x64/copilot-language-server
+      '';
     };
 }
